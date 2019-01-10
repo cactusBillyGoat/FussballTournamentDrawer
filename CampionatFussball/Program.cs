77bebb90f865ce9c00e76bf37fe2408d31224344
ciprianT <ciprianT@users.noreply.github.com>
2019-01-11 00:15:13 +0200

    Add MessageBox in Program.cs

--- CampionatFussball/Program.cs
+++ CampionatFussball/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace CampionatFussball
+﻿using System.Windows.Forms;
+
+namespace CampionatFussball
 {
     public class Program
     {
         public static void Main(string[] Args)
         {
             new Tournament().Start();
+            MessageBox.Show($"Tournament has been draw. You can find the file in: {Utilities.OutputFilePath}",
+                "Finished!", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
